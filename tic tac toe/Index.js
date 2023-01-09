a89d670ce1ad6b67b69881e1de4ab869cc2803c4
stx31 <kh1327303@gmail.com>
2023-01-09 22:19:59 +0100

    new

--- tic tac toe/Index.js
+++ tic tac toe/Index.js
@@ -1,7 +1,7 @@
 let turn = true
 const setX = new Set() //constantes//
 const setO = new Set()
-const winConditions = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]//win condi//
+const winConditions = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]//si se cumple estos criterios es la win condicion//
 const table = document.querySelector('.table')
 const buttons = [...table.children]
 table.addEventListener('click', event=>{ //al escuchar clcik  =event//
