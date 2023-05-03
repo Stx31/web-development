27126832e9c066af0f0bba9321cf549fbc5340e4
stx31 <kh1327303@gmail.com>
2023-05-03 23:17:15 +0200

    arrays

---
+++ curso/17/arrays.js
@@ -0,0 +1,8 @@
+//push - agregar al final //
+//pop  - quitar del final //
+//shift -remover del principio //
+// unshift - agregar al principio //
+
+ let cats =[ 'neko  ', 'samuel ']
+ let  horses =[ ' 1 ' , '1.2222 ']
+ cats.concat(horses)
