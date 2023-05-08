8654b7a71f625feec004766272528d8c7c7dfad5
stx31 <kh1327303@gmail.com>
2023-05-08 19:28:14 +0200

    loops

--- curso/19/19.js
+++ curso/19/19.js
@@ -1,3 +1,21 @@
 for (let i =1; i<=10; i++){
     console.log(i);
 }
+
+for (let  i =100; i>=0; i-=10){
+    console.log(i)
+}
+
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+for (let i = 1; i <= 6; i++) {
+    console.log("Da ba dee da ba daa");
+  }
+
+  const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+console.log(people[0]); // "Scooby"
+console.log(people[1]); // "Velma"
+console.log(people[2]); // "Daphne"
+console.log(people[3]); // "Shaggy"
+console.log(people[4]); // "Fred"
