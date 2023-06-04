0708a2355a756e61e5d44b3d4de6fe3987a2b2b3
stx31 <kh1327303@gmail.com>
2023-06-04 22:30:23 +0200

    new ejemls

---
+++ api/juan.js
@@ -0,0 +1,7 @@
+var nombre = "Juan";
+var edad = 25;
+var esEstudiante = true;
+
+console.log(nombre); // Imprime "Juan"
+console.log(edad); // Imprime 25
+console.log(esEstudiante); // Imprime true
