4ca4fbd28d1574fac966a271a6c266e57d60aa16
Stx31 <kh1327303@gmail.com>
2023-07-20 22:02:20 +0200

    se mover en la array de izquierda  a dereca falta de arriba a abajo

--- tabla muñeco/app.js
+++ tabla muñeco/app.js
@@ -1,27 +1,39 @@
+var cruz = 0
+
+
 function generarTabla() {
    
     const filas = parseInt(document.getElementById('inputFilas').value);
     const columnas = parseInt(document.getElementById('inputColumnas').value);
 
-  
     const tablaContainer = document.getElementById('tablaContainer');
 
-    
     const tabla = document.createElement('table');
-
 
     for (let i = 0; i < filas; i++) {
         const fila = document.createElement('tr');
         
         for (let j = 0; j < columnas; j++) {
             const celda = document.createElement('td');
-            celda.textContent = `{} ,{} `;
+            celda.innerHTML = `<div class = "celda"> x </div> `;
             fila.appendChild(celda);
-        }
+        }   
+
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'ArrowRight') {
+              moveForward();
+            } else if (event.key === 'ArrowLeft') {
+              moveBackward();
+            }
 
         tabla.appendChild(fila);
-    }
-
+    } 
+      
+    
+    
 
     tablaContainer.appendChild(tabla);
 }
+
+
+generarTabla()
