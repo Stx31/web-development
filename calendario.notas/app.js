2c2d974015385517abf5f004f9adf387f5a65b46
Stx31 <kh1327303@gmail.com>
2023-07-26 22:09:45 +0200

    calendario green contador

--- calendario.notas/app.js
+++ calendario.notas/app.js
@@ -1,34 +1,31 @@
+const fila = document.querySelector("#fila")
+const txt = document.querySelector("#texto")
+for (let index = 0; index < fila.children.length; index++) {
+    const element = fila.children[index];
+    element.addEventListener("click", () => {
+        if (element.className === "green") {
+            element.className = "red"
+        } else {
+            element.className = "green"
+        }
+    })
+    element.innerHTML += index + 1
+}
+const calcularPorcentajeClaseGreen = () => {
+    const elementos = document.querySelectorAll('.green');
+    const totalElementos = elementos.length;
+    if (totalElementos === 0) {
+      return 0;
+    }
+    const contadorGreen = elementos.length;
+    const porcentajeGreen = (contadorGreen / totalElementos) * 100;
+    return porcentajeGreen; 
+  };
 
-function mostrarResultado() {
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    let count = 0;
-    checkboxes.forEach(checkbox => {
-      if (checkbox.checked) {
-        count++;
-      }
-    });
-  
-    let porcentaje = 0;
-    if (count === 6) {
-      porcentaje = 100;
-    } else if (count === 5) {
-      porcentaje = 85;
-    } else if (count === 4) {
-      porcentaje = 60;
-    } else if (count === 3) {
-      porcentaje = 45;
-    }else if (count === 2) {
-        porcentaje = 30; }
-        else if (count === 1) {
-            porcentaje = 10; }
-  
-    
-    alert(`El porcentaje es: ${porcentaje}%`);
-  }
-  
- 
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', mostrarResultado);
-  });
-  
+  const mostrarPorcentajeEnAlerta = () => {
+    const porcentajeGreen = calcularPorcentajeClaseGreen();
+    alert(`El porcentaje es${porcentajeGreen}%`);
+  };
+
+  const botonCalcularPorcentaje = document.getElementById('calcularPorcentajeBtn');
+  botonCalcularPorcentaje.addEventListener('click', mostrarPorcentajeEnAlerta);
