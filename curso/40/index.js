addb5df8ed5b3f0d399f4b7319256b34f69694a5
Stx31 <kh1327303@gmail.com>
2023-09-02 14:54:50 +0200

    middleware (40)

---
+++ curso/40/index.js
@@ -0,0 +1,66 @@
+//modulos 
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+
+app.use(morgan('tiny'));
+
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
+})  
+// regustro de solicitudes ,Middleware '/dogs':
+app.use('/dogs', (req, res, next) => {
+    console.log("I LOVE DOGS!!")
+    next();
+})
+
+const verifyPassword = (req, res, next) => {
+    const { password } = req.query;
+    if (password === '`password123') {
+        next();
+    }
+    res.send("YOU NEED A PASSWORD!")
+}
+
+//registro de mensajes 
+
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY FIRST MIDDLEWARE!!!")
+//     return next();
+//     console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()")
+// })
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY SECOND MIDDLEWARE!!!")
+//     return next();
+// })
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY THIRD MIDDLEWARE!!!")
+//     return next();
+// })
+
+//rutas 
+app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
+    res.send('HOME PAGE!')
+})
+
+app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
+    res.send('WOOF WOOF!')
+})
+
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone')
+})
+//ruta no encontrada 
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND!')
+})
+
+//inicio servidor 
+app.listen(3000, () => {
+    console.log('App is running on localhost:3000')
+})
