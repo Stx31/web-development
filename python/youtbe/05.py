dfe98c3cefbb2ad19f6a4f0832780071d283eebc
Stx31 <kh1327303@gmail.com>
2023-09-20 15:35:19 +0200

    styles login  , home ,(inicio curso py)

---
+++ python/youtbe/05.py
@@ -0,0 +1,18 @@
+nombres = ["luke","kevin","oscar"]
+print(nombres)
+
+numeros = [12,32,43,23,45]
+print(numeros)
+
+booleans = [True,False,True]
+print(booleans)
+
+matrices = [[0,3],[0,4]]
+print(matrices)
+
+otros = ["hola",2,False]
+print(otros)
+print(nombres[0])
+print(nombres,numeros)
+
+print(nombres+numeros)
