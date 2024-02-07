874471d613d0affa1cde1b2a89355840041276f4
Stx31 <kh1327303@gmail.com>
2024-02-07 15:38:35 +0100

    php variables yoperadores

---
+++ master D/php sql/ejemplo.php
@@ -0,0 +1,8 @@
+<html>
+	<body>
+		<p>
+			<?Php echo "Hola mundo"; ?>
+		</p>
+	</body>
+</html>
+
---
+++ master D/php sql/info.php
@@ -0,0 +1,20 @@
+/*el valor puede ser true o false boleanos*/
+<?php $variables = True; ?>
+
+/*positivo Enteros (in)*/
+<?php $variable = 234; ?>
+/*negativo*/
+<?php $variable = -23; ?>
+
+/*octal*/
+<?php $variable = 0123; ?>
+/*hexadecimal*/ 
+<?php $varible = 0x1B; ?>
+/* Número de punto flotante (float)*/ 
+<?php $variable = 3.168; ?>
+
+
+/* Cadenas (string)*/ 
+
+<?php $variable = 'primera variable cadena'; ?>
+<?php $variable = "primera variable cadena"; ?>
