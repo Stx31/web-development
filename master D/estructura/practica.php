4deccb8f61944a33fa55da6fe3f20655afae42ad
Stx31 <kh1327303@gmail.com>
2024-02-08 15:17:42 +0100

    funciones

---
+++ master D/estructura/practica.php
@@ -0,0 +1,13 @@
+<html>
+ <body>
+  <p>		
+  <?php        
+    $vector = array("rojo", "azul", "verde","amarillo","gris","melocoton");
+		foreach ($vector as $var) {
+			echo "Valor: $var<br />\n";
+		}
+  ?>              
+  </p>
+ </body>
+</html>
+
