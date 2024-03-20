6cab8d6611341cc466bc239128bf50bc0273bec6
Woohaik <kh1327303@gmail.com>
2024-03-20 14:53:11 +0100

    calculadora ihpone

--- carrusel de imganes/js/app.js
+++ carrusel de imganes/js/app.js
@@ -1,44 +1,28 @@
 'use strict'
 
-const grande    = document.querySelector('.grande')
-const punto     = document.querySelectorAll('.punto')
+const grande = document.querySelector('.grande')
+const punto = document.querySelectorAll('.punto')
 
-// Cuando CLICK en punto
-    // Saber la posición de ese punto
-    // Aplicar un transform translateX al grande
-    // QUITAR la clase activo de TODOS puntos
-    // AÑADIR la clase activo al punto que hemos hecho CLICK
 
-// Recorrer TODOS los punto
-punto.forEach( ( cadaPunto , i )=> {
-    // Asignamos un CLICK a cadaPunto
-    punto[i].addEventListener('click',()=>{
+punto.forEach((cadaPunto, i) => {
 
-        // Guardar la posición de ese PUNTO
-        let posicion  = i
-        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+    punto[i].addEventListener('click', () => {
+
+        let posicion = i
+
         let operacion = posicion * -50
 
-        // MOVEMOS el grand
-        grande.style.transform = `translateX(${ operacion }%)`
 
-        // Recorremos TODOS los punto
-        punto.forEach( ( cadaPunto , i )=>{
-            // Quitamos la clase ACTIVO a TODOS los punto
+        grande.style.transform = `translateX(${operacion}%)`
+
+
+        punto.forEach((cadaPunto, i) => {
+
             punto[i].classList.remove('activo')
         })
-        // Añadir la clase activo en el punto que hemos hecho CLICK
+
         punto[i].classList.add('activo')
 
     })
 })
-
-
-
-
-
-
-
-
-
-( condicion ) ? 'hola' : 'adiós'
+ 
