e947182e36864e53615d729bafb7997f0517d85d
Stx31 <kh1327303@gmail.com>
2024-04-12 15:35:04 +0200

    footer,header,colortime,estrellasvalo

---
+++ KEHA HUB/app.js
@@ -0,0 +1,15 @@
+
+  window.addEventListener("load", function(){
+    var colors = ["radial-gradient(circle, rgb(0, 255, 255) 0%, rgba(0, 0, 0, 1) 100%)",
+                " radial-gradient(circle, rgba(32, 9, 83, 1) 0%, rgba(0, 0, 0, 1) 100%);",
+                  "radial-gradient(circle, rgb(170, 71, 28) 0%, rgba(0, 0, 0, 1) 100%);",
+                  "radial-gradient(circle, rgb(170, 28, 132) 0%, rgba(0, 0, 0, 1) 100%);"];
+    var currentIndex = 0;
+
+    function changeColor() {
+      document.body.style.background = colors[currentIndex];
+      currentIndex = (currentIndex + 1) % colors.length;
+    }
+
+    setInterval(changeColor, 3000); 
+  });
