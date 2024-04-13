7df5c602948f5495f504103db1eed1633ba2c984
Stx31 <kh1327303@gmail.com>
2024-04-13 14:50:31 +0200

    gitsubido

--- KEHA HUB/app.js
+++ KEHA HUB/app.js
@@ -1,7 +1,7 @@
 
   window.addEventListener("load", function(){
     var colors = ["radial-gradient(circle, rgb(0, 255, 255) 0%, rgba(0, 0, 0, 1) 100%)",
-                " radial-gradient(circle, rgba(32, 9, 83, 1) 0%, rgba(0, 0, 0, 1) 100%);",
+                "radial-gradient(circle, rgba(32, 9, 83, 1) 0%, rgba(0, 0, 0, 1) 100%);",
                   "radial-gradient(circle, rgb(170, 71, 28) 0%, rgba(0, 0, 0, 1) 100%);",
                   "radial-gradient(circle, rgb(170, 28, 132) 0%, rgba(0, 0, 0, 1) 100%);"];
     var currentIndex = 0;
@@ -13,3 +13,23 @@
 
     setInterval(changeColor, 3000); 
   });
+  function initMap() {
+    
+    var myLatLng = {
+      lat: -33.4372,
+      lng: -70.6506
+    };
+  
+    
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: myLatLng
+    });
+  
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Ubicación de KEHA-HUB'
+    });
+  }
+  
