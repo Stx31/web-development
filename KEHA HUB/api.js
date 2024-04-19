4ed8ea0eaa3eb6865c9ae02830ba5dce487bb30b
Stx31 <kh1327303@gmail.com>
2024-04-19 14:02:17 +0200

    header bostrap , formulairio , maps

---
+++ KEHA HUB/api.js
@@ -0,0 +1,19 @@
+ 
+    function initMap() {
+      
+        var myLatLng = {lat: -34.397, lng: 150.644};
+
+        
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: myLatLng,
+            zoom: 8 
+        });
+
+        
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Mi ubicación'
+        });
+    }
+
