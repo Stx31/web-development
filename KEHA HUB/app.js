cb666890c6e1dd208ce31fbdf7357f6f049461c2
Stx31 <kh1327303@gmail.com>
2024-04-23 15:09:52 +0200

    mapa santander

--- KEHA HUB/app.js
+++ KEHA HUB/app.js
@@ -1,27 +1,12 @@
-// Initialize and add the map
-let map;
-
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
+function iniciarMap() {
+  const santander = { lat: 43.4623, lng: -3.8099 }; 
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: santander, 
   });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: position,
-    title: "Uluru",
+  const marker = new google.maps.Marker({
+    position: santander, 
+    map: map, 
+    title: "Santander, España", 
   });
 }
-
-initMap();
