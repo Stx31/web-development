282479d3edd3b8b6e4dc8e8194bbe62a60a5fad3
Stx31 <kh1327303@gmail.com>
2024-05-12 15:01:47 +0200

    presupuesto html

--- master D/proyecto css y html/js/app.js
+++ master D/proyecto css y html/js/app.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("contact-form");
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault(); 
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-    
+
         const nombre = document.getElementById("nombre").value;
         const email = document.getElementById("email").value;
         const telefono = document.getElementById("telefono").value;
         const mensaje = document.getElementById("mensaje").value;
         const politica = document.getElementById("politica").checked;
 
-    
+
         const datos = {
             nombre: nombre,
             email: email,
@@ -20,12 +20,12 @@ document.addEventListener("DOMContentLoaded", function() {
             politica: politica
         };
 
-        
+
         const datosJSON = JSON.stringify(datos);
 
         localStorage.setItem("formularioDatos", datosJSON);
 
-        
+
         alert("Los datos del formulario han sido guardados en el localStorage.");
     });
 });
