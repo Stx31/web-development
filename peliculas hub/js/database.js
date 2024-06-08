69b9031103b6dc6abb9bab0164f1780d630f5ca2
Stx31 <kh1327303@gmail.com>
2024-06-08 15:32:15 +0200

    usuraios node , base de datos

---
+++ peliculas hub/js/database.js
@@ -0,0 +1,9 @@
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(() => {
+  db.run("CREATE TABLE users (id INTEGER PRIMARY KEY, email TEXT, username TEXT, password TEXT)");
+});
+
+module.exports = db;
