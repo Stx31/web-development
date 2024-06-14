7456766d6ac8b3a2f22b65ce41ba66b69bbbfac5
Stx31 <kh1327303@gmail.com>
2024-06-14 15:57:57 +0200

    numeros clase udemy

---
+++ repaso cursoi/numeros.js
@@ -0,0 +1,20 @@
+for (let i = -100; i <= 100; i++) {
+    console.log(i);
+}
+
+
+
+for (let i = -100; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(`Número par: ${i}`);
+    } else {
+        console.log(`Número impar: ${i}`);
+    }
+}
+
+let myLuckyNumber = 37;
+let octopusLimbs = 8;
+
+
+let resultado = (10 + 5) * (20 - 8) / 4;
+console.log("Resultado:", resultado); 
