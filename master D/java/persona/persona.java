419e70f024de5f8221fce9ae18301c6bd9d67fc4
Stx31 <kh1327303@gmail.com>
2024-07-12 16:11:44 +0200

    Persona java

---
+++ master D/java/persona/persona.java
@@ -0,0 +1,33 @@
+public class Persona {
+
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+ 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+   
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellidos: " + apellidos);
+        System.out.println("Edad: " + edad);
+    }
+}
--- master D/variables y operadores/variables/notas.js
+++ master D/variables y operadores/variables/notas.js
@@ -1,18 +1,23 @@
 function calcularPromedio(nota1, nota2, nota3, nota4, nota5) {
+    
+    const notas = [nota1, nota2, nota3, nota4, nota5];
+    
+    
+    notas.forEach((nota, index) => {
+        console.log(`Tipo de dato de nota${index + 1}: ${typeof nota}`);
+    });
 
-    console.log("Tipo de dato de nota1: " + typeof nota1);
-    console.log("Tipo de dato de nota2: " + typeof nota2);
-    console.log("Tipo de dato de nota3: " + typeof nota3);
-    console.log("Tipo de dato de nota4: " + typeof nota4);
-    console.log("Tipo de dato de nota5: " + typeof nota5);
-    var promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+
+    const suma = notas.reduce((acc, nota) => acc + nota, 0);
+    const promedio = suma / notas.length;
     return promedio;
 }
-var nota1 = 8.0;
-var nota2 = 7.5;
-var nota3 = 9.0;
-var nota4 = 6.5;
-var nota5 = 8.5;
 
-var promedioNotas = calcularPromedio(nota1, nota2, nota3, nota4, nota5);
+const nota1 = 8.0;
+const nota2 = 7.5;
+const nota3 = 9.0;
+const nota4 = 6.5;
+const nota5 = 8.5;
+
+const promedioNotas = calcularPromedio(nota1, nota2, nota3, nota4, nota5);
 console.log("El promedio de las notas es: " + promedioNotas);
