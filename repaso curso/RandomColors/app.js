e28c3e72026fbb40d6198995cc3eb3e5504a6ebb
Stx31 <kh1327303@gmail.com>
2024-08-03 15:53:25 +0200

    random color

--- repaso curso/RandomColors/app.js
+++ repaso curso/RandomColors/app.js
@@ -5,12 +5,22 @@ button.addEventListener('click', function () {
     const newColor = makeRandColor();
     document.body.style.backgroundColor = newColor;
     h1.innerText = newColor;
-})
+    h1.style.color = invertColor(newColor);
+    button.style.backgroundColor = newColor;
+    button.style.color = invertColor(newColor);
+});
 
 const makeRandColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
     return `rgb(${r}, ${g}, ${b})`;
-}
+};
 
+const invertColor = (rgb) => {
+    const rgbValues = rgb.match(/\d+/g).map(Number);
+    const r = 255 - rgbValues[0];
+    const g = 255 - rgbValues[1];
+    const b = 255 - rgbValues[2];
+    return `rgb(${r}, ${g}, ${b})`;
+};
